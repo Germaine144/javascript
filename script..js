a5026a8b0aee65ee5b0e4224bbb005de2bc15d45
Germaine144 <umuhiregermaine12@gmail.com>
2024-12-12 14:35:19 +0200

    function of two numbers that make sum

---
+++ script..js
@@ -0,0 +1,18 @@
+1.//Write a function that takes two numbers (a and b) as argument. Sum a and b. Return the result
+function myFunction(a,b){
+
+return a+b;
+
+}
+//Test Cases:
+Attempted: 289071
+Solved: 163315
+myFunction(1,2)
+Expected
+3
+myFunction(1,10)
+Expected
+11
+myFunction(99,1)
+Expected
+100
